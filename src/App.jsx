ee0ec5de124fd7488fd44e42f7a43e03ecd04165
Prabhnoor Ughra <prabhnoor.ughra@gmail.com>
2025-08-27 15:06:52 -0400

    Create Routes and Implement them with Outlet

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,29 @@ import { useState, useEffect, createContext, } from 'react'
 import { Outlet, useLocation, useNavigate } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import './App.css'
+import NavBar from './components/NavBar';
+
+const apiURL = import.meta.env.VITE_API_URL;
 
 function App() {
+  //log a visit to the site
+  useEffect(() => {
+    fetch(`${apiURL}/`, {
+      method: "POST"
+    })
+  }, []);
 
   return (
     <>
-      <h1>
-        Our First Test
-      </h1>
-      <Outlet />
+        <div className='h-screen flex flex-column w-screen'>
+          <div className='h-screen w-screen grid grid-cols-1 grid-rows-[auto_1fr]
+        text-black'>
+            <NavBar />
+            <div className="bg-[#132F57] overflow-y-auto">
+              <Outlet />
+            </div>
+        </div>
+        </div>
     </>
   )
 }
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,5 +1,10 @@
 import App from "./App";
 import ErrorPage from "./components/ErrorPage"
+import HomePage from "./components/HomePage";
+import ApplyPage from "./components/ApplyPage";
+import ContactsPage from "./components/ContactsPage";
+import TeamPage from "./components/TeamPage";
+import EventsPage from "./components/EventsPage";
 
 
 
@@ -11,7 +16,11 @@ const routes = [
         element: <App />,
         errorElement: <ErrorPage />,
         children: [
-
+            {index: true,           element: <HomePage />},
+            {path: "apply",         element: <ApplyPage />},
+            {path: "contacts",      element: <ContactsPage />},
+            {path: "team",          element: <TeamPage />},
+            {path: "events",        element: <EventsPage />},
         ],
     },
     {
