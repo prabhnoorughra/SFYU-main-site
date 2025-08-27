229d40f587f0daf861f73c609b682ab114596cad
Prabhnoor Ughra <prabhnoor.ughra@gmail.com>
2025-08-27 17:12:32 -0400

    Fix Team Members Programs

--- src/components/TeamPage.jsx
+++ src/components/TeamPage.jsx
@@ -13,7 +13,7 @@ const execs = [
     name: "Brianna Callisto",
     role: "Vice-President",
     year: "Third",
-    program: "Biology",
+    program: "Honours BSc Biology",
     about:
       `My name is Brianna, and I am currently pursuing a degree in Biology at York University. Outside of academics, I enjoy staying active through running and pilates, and I also love unwinding with a good book. A fun fact about me is that I’m always on the lookout for hidden nature trails to explore. I find being outdoors helps me recharge and stay balanced. This year, I’m excited to be serving as a Vice President of the Healthcare Society at YorkU, where I get to connect with like-minded peers and contribute to meaningful initiatives. I am deeply passionate about helping people, and I hope to carry that passion forward into a future career in healthcare, where I can make a lasting impact on others’ lives.
         What I love most about York University is the supportive and welcoming community that makes it easy to feel at home. Being part of such an environment has encouraged me to grow not only as a student but also as a leader and a person. Looking ahead, I am motivated to continue learning, building connections, and preparing myself for a role where I can combine my love for science with my commitment to helping others.`,
@@ -22,7 +22,7 @@ const execs = [
     name: "Suheera Siddiqui",
     role: "Vice-President",
     year: "Fifth",
-    program: "Global Health",
+    program: "Honours BSc Global Health",
     about:
       `My name is Suheera, and I am currently pursuing a Bachelor’s degree in Global Health with a minor in Biology at York University. I am passionate about bridging science, healthcare, and community impact, which has led me to be actively involved in various academic, clinical, and leadership initiatives. This year, I am serving as Vice President of STEM Fellowship, where I help organize programs that empower students through research and innovation. In my free time, I enjoy going on walks, painting, and spending time with my cat, Mila. One of my favourite things about YorkU is its diverse student community, which inspires creativity and leadership. Looking forward to a successful year!`,
   },
@@ -30,7 +30,7 @@ const execs = [
     name: "Sania Khoso",
     role: "Indicum Lead",
     year: "Fourth",
-    program: "Kinesiology and Health Science",
+    program: "Honours BSc Kinesiology and Health Science",
     about:
       `My name is Sania Khoso, and I am excited to be serving as the Indicium Lead for the STEM Fellowship Club at York University. I am entering my fourth year of undergraduate studies in Kinesiology and Health Science. I plan to pursue a career as either an ultrasound technician or a physician's assistant after my undergraduate studies. Some of my hobbies include reading, painting, and going for long walks. One of my favourite things about York University is its diverse communities, as there are a variety of clubs to join and events to meet new people. I also love York's campus, especially the outdoor seating areas and cherry blossoms that bloom each May.`,
   },
@@ -38,7 +38,7 @@ const execs = [
     name: "Bahar Saroya",
     role: "Junior Indicum Lead",
     year: "Fourth",
-    program: "Kinesiology and Health Science",
+    program: "Honours BSc Kinesiology and Health Science",
     about:
       `My name is Bahar and I am currently studying Kinesiology, entering my fourth year this fall at York University. In my free time, I enjoy painting, reading, and spending quality time with my family and friends. Over the past year, I had the opportunity to work at a physiotherapy clinic, where I supported physiotherapists and worked closely with patients. This experience helped me develop strong communication and hands-on skills while deepening my passion for helping others. In the future, I hope to pursue a career in which I can continue supporting individuals in improving their physical well-being. One of the things I value most about York University is its supportive and diverse community, as well as the many opportunities it offers to get involved in various clubs, meet new people, and grow both academically and personally.`,
   },
@@ -46,7 +46,7 @@ const execs = [
     name: "Seleena Yevenes",
     role: "REO Lead",
     year: "Fourth",
-    program: "Biology",
+    program: "Honours BSc Biology",
     about:
       `My name is Seleena Yevenes (she/her), and I am honoured to serve as the Research Exploration Opportunity (REO) Lead of STEM Fellowship at York University for the 2025–2026 academic year. I am currently in my fourth year of the Honours BSc Biology program at York University. Alongside this role, my other commitment at York University this academic year includes working in a research lab. During my free time, I enjoy spending time with my sister, going for runs, and learning new recipes. Throughout my years at York University, I have established profound relationships amongst peers, teaching assistants, and professors. I have come to learn a great deal about myself and others, which has led me toward meaningful experiences. In the future, I aspire to pursue a master’s degree in animal biology, with the goal of working with animals and contributing to research in the field.`,
   },
@@ -54,7 +54,7 @@ const execs = [
     name: "Maneet Batajoo",
     role: "REO Coordinator",
     year: "Fourth",
-    program: "Biology",
+    program: "Honours BSc Biology",
     about:
       `My name is Maneet Batajoo and I am a fourth-year Biology student at York University. I enjoy combining my passion for science with activities that keep me engaged outside the classroom, such as swimming, tennis, and golf. My years of experience in aquatics taught me the value of leadership and mentorship, as I had the opportunity to guide students of all ages. At York, I appreciate the diverse community and the chance to learn from people with different perspectives. I am especially interested in how biology and technology can come together to improve health and quality of life. Looking ahead, I hope to build a career in biomedical research and global health, where I can contribute to meaningful innovations while staying connected to the communities I serve.`,
   },
@@ -62,7 +62,7 @@ const execs = [
     name: "Alankriti Thakur",
     role: "Events Lead",
     year: "Third",
-    program: "Biology",
+    program: "Honours BSc Biology",
     about:
       `I am Alankriti Thakur (she/her), and I’m an Honours Bachelor of Science Biology student at York University with a passion for science, leadership, and creating meaningful opportunities for growth. This year, I’m thrilled to serve as the Event Lead for the STEM Fellowship Club, where I create and run events that spark curiosity, build connections, and inspire students to explore their potential in STEM. My journey with the club started last year as Event Coordinator, and that experience taught me the power of collaboration, adaptability, and creative problem-solving. Outside of STEM Fellowship, I’m involved in other campus initiatives that let me combine my love for science with advocacy and community engagement. I am passionate about creating inclusive spaces where students can learn from one another, share their experiences, and feel supported in their academic and professional journeys. 
       In my personal life, I enjoy discovering new hobbies and am always open to exploring fresh opportunities and experiences that broaden my perspective. York University’s vibrant, diverse, and supportive community has shaped me in countless ways, and I’m grateful for the people and experiences that have inspired my journey so far. In the future, I plan to pursue a career in healthcare, where I can merge my scientific expertise with compassion to improve lives and make a lasting impact.`,
@@ -71,7 +71,7 @@ const execs = [
     name: "Gurneet Chahal",
     role: "Events Lead",
     year: "Third",
-    program: "Kinesiology and Health Science",
+    program: "Honours BSc Kinesiology and Health Science",
     about:
       `My name is Gurneet Chahal (She/Her), and I am honored to be a part of the 2025–2026 STEM Fellowship team as an Events Lead. I am currently in my third year at York University, pursuing a degree in Kinesiology and Health Science, with the goal of building a future career in the health field. In addition to STEM Fellowship, I also serve as President of GlamourGals and Events Lead of Healthcare Society at York, where I enjoy creating meaningful opportunities for student engagement and growth. Outside of school, I dedicate my time to various volunteer and work commitments, and in my free time, I love spending time with my dog, hanging out with my sister, or reading books. One of the highlights of my York experience has been the friendships I’ve built along the way, as well as the strong sense of community. Beyond that, I truly value the beautiful campus, the connections I’ve made, and the many opportunities York has provided me to learn and grow.`,
   },
@@ -79,7 +79,7 @@ const execs = [
     name: "Khushpreet Deol",
     role: "Events Coordinator",
     year: "Third",
-    program: "Kinesiology and Health Science",
+    program: "Honours BSc Kinesiology and Health Science",
     about:
       `Hi my name is Khushpreet Deol. I am currently in my third year of Kinesiology and Health Science. I have very excited to be apart of the events team this year. In my free time I love to bake, garden and sew. I am apart of Health care society club and the Sikh associations club. I also volunteer at the hospital where I get to help patients, help relive any If their concerns and make their experience at the hospital more enjoyable. I love hanging out with my friends and family and enjoying good food and music together. We love going to concerts and trying new restaurants.`,
   },
@@ -87,7 +87,7 @@ const execs = [
     name: "Sajal Sandhu",
     role: "Marketing Director",
     year: "Second",
-    program: "Computer Engineering",
+    program: "Honours BEng Computer Engineering",
     about:
       `My name is Sajal Sandhu (she/her), and I am currently pursuing a Bachelor of Engineering in Computer Engineering at York University. This year, I am honoured to serve as the Research Exploration Opportunity (REO) Coordinator for STEM Fellowship at YorkU, where I support student involvement in research and foster opportunities for growth. One of my favourite aspects of York University has been the meaningful relationships I have created with peers, professors, and mentors, which have helped me both academically and personally. These connections have provided me with encouragement and collaboration, while also challenging me to think critically and push beyond my comfort zone. Alongside my academic and leadership commitments, I founded and run setswsage, a nail brand where I design and create custom press-ons and gel x extensions. This venture has allowed me to combine creativity with business skills, while also teaching me discipline, time management, and how to connect with clients on a personal level. I value taking on challenges as opportunities to learn and grow, whether in engineering projects, leadership roles, or entrepreneurial pursuits. Looking ahead, I aspire to pursue a Master’s in Business to complement my technical foundation, with the goal of building a career that merges engineering, innovation, and leadership to make a lasting impact.`,
   },
@@ -95,7 +95,7 @@ const execs = [
     name: "Yanti Mulyani",
     role: "Marketing Director",
     year: "Fifth",
-    program: "Biomedical Science",
+    program: "Honours BSc Biomedical Science",
     about:
       `Hello everyone! my name is Yanti (read: Yantee) Mulyani (she/her) and I am very excited to be the marketing manager of STEM FELLOWSHIP at York University for 2025/2026 academic year. I am currently doing Biomedical Science at York and hoping to be able to graduate this upcoming summer (very excited!). Alongside of being the marketing manager of SFYU, I also involved in many other student organizations such as York University Indonesian Student Association (currently serving as a President), part of Science Student Ambassadors, and also serve as a volunteer at the diagnostic imaging department at Brampton Civic Hospital. Some interesting fact about me is that I am fluent in about 3 languages, two of those are Indonesian and Sundanese aside from English, in addition, I am also a very introverted person and thrive in working alone. I have a huge passion for lab works hence why one of my favourite courses was organic chemistry! and I am hoping to be able to pursue a master’s degree in Medical Lab Technician once I graduate. One of the thing that I love about York is the opportunities that came from connections that I made throughout my academic year. Meeting many people with the same passion as me has been thrilling and opens up many great opportunities that leads to great experiences.`,
   },
